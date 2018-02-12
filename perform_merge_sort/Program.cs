d665d1fb09eaf1b48d10ecbb9a07c74adfd0437e
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-12 21:07:51 +1100

    Add Main

--- perform_merge_sort/Program.cs
+++ perform_merge_sort/Program.cs
@@ -45,5 +45,16 @@ namespace perform_merge_sort
                 MergeMethod(numbers, left, (mid + 1), right);
             }
         }
+
+        static void Main(string[] args)
+        {
+            int[] numbers = { 3, 8, 7, 5, 2, 1, 9, 6, 4 };
+            int len = 9;
+            Console.WriteLine("MergeSort :");
+            SortMethod(numbers, 0, len - 1);
+            for (int i = 0; i < 9; i++)
+                Console.WriteLine(numbers[i]);
+            Console.Read();
+        }
     }
 }
